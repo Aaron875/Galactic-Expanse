8da41c108909adbb0e32e49bc71bf50601b00296
Matt Izzo (RIT Student) <mmi@izzo.me>
2022-02-18 13:29:13 -0500

    Enemy now only checks for attacks every 2 seconds

--- galacticExpanse/Assets/Scripts/EnemyManager.cs
+++ galacticExpanse/Assets/Scripts/EnemyManager.cs
@@ -8,7 +8,9 @@ public class EnemyManager : MonoBehaviour
     [SerializeField] private List<Building> buildings; //All the bases
     private GameManager gameManager;
     private SquadManager squadManager;
-    int randomNum;
+
+    private int randomNum; //Used for holding randoms
+    private float timer; //Used for incrementing unit counts
     //int attackCounter; - Variable just for testing, basically can be ignored
 
     // Start is called before the first frame update
@@ -16,21 +18,57 @@ public class EnemyManager : MonoBehaviour
     {
         buildings = GetComponent<BuildingMgr>().Buildings;
         squadManager = GetComponent<SquadManager>();
+        timer = 0;
 
-    randomNum = 10000;
+        randomNum = 10000; //This is just a holder value, makes sure nothing runs frame 1
         //attackCounter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < buildings.Count; i++)
+        for(int i = 0; i < buildings.Count; i++) //Loops through all buildings
         {
 
-
-            if (buildings[i].Alignment == "E" && buildings[i].NumUnits >= 15)
+            if (buildings[i].Alignment == "E" && buildings[i].NumUnits >= 15) //If a building is an enemy building and has 15 or more units
             {
-                randomNum = Random.Range(0, 10000);
+
+                timer += Time.deltaTime;
+
+                //Change number to change how quickly buildings gain units
+                if (timer >= 2)
+                {
+                    randomNum = Random.Range(0, 1000); //1000 is just randomly there, can be changed
+                    if (randomNum < buildings[i].NumUnits) //If the number is less than the unit count it attacks, more aggressive the more units it has
+                    {
+
+                        int target = (int)Random.Range(0, buildings.Count); //This is for the target of the attack
+                        //Debug.Log(target);
+
+                        while (target == i) //This ensures that it will not target itself
+                        {
+                            target = (int)Random.Range(0, buildings.Count);
+                            //Debug.Log(target);
+                        }
+
+                        //This makes sure that the unit numbers will not be weird if it has an odd number
+                        if (buildings[i].NumUnits % 2 == 0)
+                        {
+                            buildings[i].NumUnits = buildings[i].NumUnits / 2;
+                            squadManager.CreateSquad(1, 0, buildings[i].NumUnits, buildings[i].transform.position, buildings[target]);
+                        }
+                        else
+                        {
+                            buildings[i].NumUnits = buildings[i].NumUnits / 2;
+                            squadManager.CreateSquad(1, 0, buildings[i].NumUnits, buildings[i].transform.position, buildings[target]);
+                            buildings[i].NumUnits++;
+                        }
+
+                    }
+                    timer = 0;
+                }
+
+                /*randomNum = Random.Range(0, 10000);
                 if (randomNum * 10 < buildings[i].NumUnits) // The *20 is the nerf the enemy. The higher the number the less active it will be.
                 {
 
@@ -56,7 +94,7 @@ public class EnemyManager : MonoBehaviour
                         buildings[i].NumUnits++;
                     }
 
-                }
+                }*/
             }
         }
     }

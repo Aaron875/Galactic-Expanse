f3f5ddb81af25fe22b2dc1aef0d2ed9fd7f770f3
Kevin <kjl8898@rit.edu>
2022-02-24 16:02:55 -0500

    Update GameManager.cs

--- galacticExpanse/Assets/Scripts/GameManager.cs
+++ galacticExpanse/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,14 +9,27 @@ public class GameManager : MonoBehaviour
     public GameObject pStart; //Player Start
     public GameObject eStart; //Enemy Start
     private float timer; //Used for incrementing unit counts
+    [SerializeField] private bool isTutorialActive = false;
     [SerializeField] private bool isPaused = true;
 
     // Managers
     private EnemyManager enemyManager;
+    private BuildingMgr buildingManager;
+
+    // Tutorial Objects
+    [SerializeField] private GameObject tutorialStep1;
+    [SerializeField] private GameObject tutorialStep2;
 
     // Start is called before the first frame update
     void Start()
     {
+        if(SceneManager.GetActiveScene().name == "Tutorial")
+        {
+            isTutorialActive = true;
+/*            tutorialStep1.SetActive(true);
+            tutorialStep2.SetActive(false);*/
+        }
+
         enemyManager = GetComponent<EnemyManager>();
 
         timer = 0;
@@ -36,8 +50,15 @@ public class GameManager : MonoBehaviour
 
         if(!isPaused)
         {
-            Debug.Log("Updating AI...");
             enemyManager.UpdateAI();
+        }
+
+        if(isTutorialActive)
+        {
+/*            if (buildingManager.Buildings[2].Alignment == "P")
+            {
+                tutorialStep1.SetActive(false);
+            }*/
         }
     }
 

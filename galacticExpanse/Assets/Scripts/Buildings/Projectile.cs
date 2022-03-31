6316477ac8e00e8fc1a866b3609e3685d1f9575d
Kevin Laporte <kjl8898@rit.edu>
2022-03-31 18:03:54 -0400

    Added Projectile Particles

--- galacticExpanse/Assets/Scripts/Buildings/Projectile.cs
+++ galacticExpanse/Assets/Scripts/Buildings/Projectile.cs
@@ -7,6 +7,7 @@ public class Projectile : MonoBehaviour
     [SerializeField] private Squad target;
     [SerializeField] private float speed = 80;
     [SerializeField] private int damage = 2;
+    [SerializeField] private GameObject explosionGO;
 
     public Squad Target
     {
@@ -16,18 +17,30 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(target == null)
+        try
+        {
+            if (target == null)
+            {
+                Destroy(this.gameObject);
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
+            transform.up = target.transform.position - transform.position;
+
+            if (Vector2.Distance(transform.position, target.transform.position) <= 20)
+            {
+                target.DamageSquad(damage);
+                Destroy(this.gameObject);
+            }
+        }
+        catch
         {
             Destroy(this.gameObject);
         }
+    }
 
-        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
-        transform.up = target.transform.position - transform.position;
-
-        if (Vector2.Distance(transform.position, target.transform.position) <= 20)
-        {
-            target.DamageSquad(damage);
-            Destroy(this.gameObject);
-        }
+    private void OnDestroy()
+    {
+        Instantiate(explosionGO, transform.position, Quaternion.identity);
     }
 }
---
+++ galacticExpanse/Assets/Scripts/DestoryAfterTimer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryAfterTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroySelf", 1);
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
